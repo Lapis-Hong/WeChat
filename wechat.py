ef977830c1f3268954c6069b458bd1a6499fda10
lapis-hong <dhq1993@qq.com>
2017-11-07 18:19:56 +0800

    first commit

---
+++ wechat.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: lapis-hong
+# @Date  : 2017/11/7
+import os
+import datetime as dt
+import random
+import sys
+import logging
+
+import itchat
+from apscheduler.schedulers.background import BackgroundScheduler
+import warnings
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+logging.basicConfig()
+warnings.filterwarnings("ignore")  # 忽略WARNING信息
+
+NAME = 'Lapis-Hong'
+
+
+def send():
+    """发送消息函数"""
+    meet_date = dt.date(2016, 5, 15)  # 跟某人相识日期
+    today_date = dt.date.today()  # 今天日期
+    pass_days = (today_date - meet_date).days  # 相识天数
+    now = dt.datetime.now()  # 获取当前时间  datetime.datetime(2017, 11, 7, 15, 28, 23, 926233)
+    now_time = now.strftime('%H:%M')  # 格式化时间，截取小时和分钟字段
+
+    # send text
+    content = ['Have you learned Python today?', 'best regards from Mr.H', 'Hello, little Jun',
+               'Have you write the words?', 'Drink some water', 'Keep smiling', 'Do not look the phone',
+               'Eat some thing', 'I miss U', 'How are you', 'Nice to meet you', 'Hi, little pig', 'Are you busy?'
+               'Time to sleep', 'See you soon', 'Do not miss me', 'What are you doing?',
+               ]  # 发送消息内容列表
+    content_sample = random.choice(content)  # 随机选择消息列表中一条消息
+    message = 'Today is our {} day, now it is {}. {}'.format(pass_days, now_time, content_sample)  # 发送的完整消息
+    itchat.send(msg=message, toUserName=user_name)
+    print('Already send the message: {}'.format(message))
+
+    # send image
+    image_dir = os.path.abspath('.') + '/image'  # 获取存储的图片路径 '/Users/lapis-hong/Documents/Python/untitled1/image'
+    image_filenames = [f for f in os.listdir(image_dir) if f.endswith('.jpeg')]  # 获取图片路径下所有以.jpg结尾的文件名列表
+    image_sample = random.choice(image_filenames)  # 随机选择一张图片
+    image_path = os.path.join(image_dir, image_sample)
+    itchat.send("@img@%s" % image_path, toUserName=user_name)  # 发送图片
+    print('Already send a image: {}'.format(image_path))
+
+    # 递归调用 my_scheduler
+    next_time = now + dt.timedelta(minutes=30)  # 下一次发送消息时间, 输出 datetime.datetime(2017, 11, 7, 14, 21, 44, 269256)
+    next_time = next_time.strftime('%Y-%m-%d %H:%M:%S')  # 格式化， 输出 '2017-11-07 14:21:44'
+    my_scheduler(next_time)  # 启用下一次定时
+
+
+def my_scheduler(run_time):
+    """定时函数"""
+    scheduler = BackgroundScheduler()  # 实例化
+    scheduler.add_job(send, 'date', run_date=run_time)
+    scheduler.start()
+
+
+# 自动回复消息函数
+# @itchat.msg_register(itchat.content.TEXT)
+# def text_reply(msg):  # msg is a dict
+#     if msg['FromUserName'] == user_name:  # 自动回复某人信息
+#         return '爸爸正忙，请稍后在拨，爸爸不在，好好学习，爸爸回家要监督你，嘻嘻嘻'
+
+
+if __name__ == '__main__':
+    # itchat.auto_login(enableCmdQR=False)  # True命令行二维码, 默认False是图片格式二维码
+    itchat.auto_login(hotReload=True)  # 方便调试，不用每次都扫二维码
+    # from friends name to get user id
+    users_info = itchat.search_friends(name=NAME)
+    print('User info: {}'.format(users_info))
+    user_name = users_info[0]['UserName']  # User id @8935ca1e425e42432f5b7b8bb3f9244d1a9918ee954bdbafe8afc92a24dd1921
+    print('User Name: {}'.format(user_name))
+
+    send()  # 会递归调用send()
+    itchat.run()  # 跑微信服务
+
+
---
+++ wechat_weather.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: lapis-hong
+# @Date  : 2017/11/7
+import bs4
+import urllib
+import sys
+import datetime as dt
+import logging
+
+import itchat
+from apscheduler.schedulers.background import BackgroundScheduler
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+logging.basicConfig()
+
+
+def get_weather():
+    wheatherHtml = urllib.urlopen('http://www.weather.com.cn/weather/101010100.shtml').read().decode('utf8')
+    webpage = bs4.BeautifulSoup(wheatherHtml)
+    body = webpage.body
+    data = body.find('div', {'id': '7d'})
+    ul = data.find('ul')
+    li = ul.find_all('li')
+    final = []
+    for day in li:  # 对每个li标签中的内容进行遍历
+        temp = []
+        date = day.find('h1').string  # 找到日期
+        temp.append(date)
+        inf = day.find_all('p')  # 找到所有p标签
+        temp.append(inf[0].string)  # 将天气状况加到temp中
+        if inf[1].find('span') is None:
+            temperature_highest = None
+        else:
+            temperature_highest = inf[1].find('span').string
+            # temperature_highest = temperature_highest.replace('℃', '')  # 到了晚上网站会变，最高温度后面也有个℃
+        temperature_lowest = inf[1].find('i').string  # 找到最低温
+        # temperature_lowest = temperature_lowest.replace('℃', '')
+        temp.append(temperature_highest)  # 将最高温添加到temp中
+        temp.append(temperature_lowest)  # 将最低温添加到temp中
+        final.append(temp)
+    if int(final[1][3].replace('℃', '')) <= -10:
+        word = '<明天非常非常非常冷，宝宝穿上所有能穿的！>'
+    elif -10 < int(final[1][3].replace('℃', '')) <= -5:
+        word = '<明天很冷很冷很冷，宝宝多穿多穿多穿！>'
+    elif -5 < int(final[1][3].replace('℃', '')) <= 0:
+        word = '<明天小冷，宝宝多穿点哦~>'
+    else:
+        word = '<明天不太冷，宝宝随意穿~>'
+    tom = final[1]
+    tom[2] = '最高气温:' + tom[2]
+    tom[3] = '最低气温:' + tom[3]
+    return ' '.join(tom) + '  ' + word
+
+
+def send():
+    itchat.send(msg=content, toUserName=user_name)
+    now = dt.datetime.now()
+    next_time = now + dt.timedelta(days=1)  # 下一次发送消息时间, 输出 datetime.datetime(2017, 11, 7, 14, 21, 44, 269256)
+    next_time = next_time.strftime('%Y-%m-%d %23:%30:%00')  # 明天的23：30再次发送消息
+    my_scheduler(next_time)  # 启用下一次定时
+
+
+def my_scheduler(run_time):
+    """定时函数"""
+    scheduler = BackgroundScheduler()  # 实例化
+    scheduler.add_job(send, 'date', run_date=run_time)
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    itchat.auto_login(hotReload=True)  # 方便调试，不用每次都扫二维码
+    users_info = itchat.search_friends(name='Lapis-Hong')
+    user_name = users_info[0]['UserName']  # User id @8935ca1e425e42432f5b7b8bb3f9244d1a9918ee954bdbafe8afc92a24dd1921
+    content = get_weather()
+    send()
+    itchat.run()
+
+
